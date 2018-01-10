585a60d926b1d00bbde9bc274949b1616bf1884a
PK Banks <pkbanks12@gmail.com>
2018-01-10 12:13:33 -0500

    end of rails-views-2 lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+# routes.rb
   
   root 'home#index'
+
+  resources :movies
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
